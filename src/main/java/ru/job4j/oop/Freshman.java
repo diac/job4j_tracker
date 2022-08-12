206cf452e348f69dac00797a2908b14b032fac98
diac <nikolai.gladkikh@gmail.com>
2022-08-12 18:57:22 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/oop/Freshman.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Freshman extends Student {
+
+    public static void main(String[] args) {
+        Student student = new Freshman();
+        Object obj = new Student();
+    }
+}

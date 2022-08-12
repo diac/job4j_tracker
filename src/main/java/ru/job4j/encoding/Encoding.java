b4a074dacd804d9ecdd20ea57ee77f29d5b0bc29
diac <nikolai.gladkikh@gmail.com>
2022-08-12 17:22:52 +0300

    Проверка кодировки

---
+++ src/main/java/ru/job4j/encoding/Encoding.java
@@ -0,0 +1,8 @@
+package ru.job4j.encoding;
+
+public class Encoding {
+
+    public static void main(String[] args) {
+        System.out.println("Съешь еще этих мягких французских булок, да выпей чаю");
+    }
+}

78e8dc519e81e54bcf1f2a4c2cae8a0f2c6f61af
diac <nikolai.gladkikh@gmail.com>
2022-08-13 08:49:09 +0300

    Исправить ошибки стиля: 5. Tracker - хранилище [#396 #331414]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,26 +14,19 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] result = new Item[items.length];
-        int resultSize = 0;
-        for (int i = 0; i < items.length; i++) {
-            if (items[i] != null) {
-                result[resultSize++] = items[i];
-            }
-        }
-        return Arrays.copyOf(result, resultSize);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
         Item[] result = new Item[items.length];
-        int resultSize = 0;
+        int count = 0;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (item.getName().equals(key)) {
-                result[resultSize++] = item;
+                result[count++] = item;
             }
         }
-        return Arrays.copyOf(result, resultSize);
+        return Arrays.copyOf(result, count);
     }
 
     public Item findById(int id) {

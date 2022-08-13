c409a7929a6b4792cebd05c4a141a626b9fb1403
diac <nikolai.gladkikh@gmail.com>
2022-08-13 12:59:26 +0300

    Убрать вложенный цикл: 2. Scanner и чтение числа из консоли. [#179518 #331500]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -10,24 +10,23 @@ public class Matches {
         boolean turn = true;
         int count = 11;
         while (count > 0) {
-            String player = turn ? "Первый игрок " : "Второй игрок";
+            String player = turn ? "Первый игрок" : "Второй игрок";
             int matches;
-            do {
-                System.out.println("Осталось спичек: " + count);
-                if (count >= 3) {
-                    System.out.println(player + " введите число от 1 до 3:");
-                } else if (count == 2) {
-                    System.out.println(player + " введите число от 1 до 2:");
-                } else {
-                    System.out.println(player + ", введите 1, чтобы победить!");
-                }
-                matches = Integer.parseInt(input.nextLine());
-                if (!((matches <= count) && (matches >= 1) && (matches <= 3))) {
-                    System.out.println("Неверный ход!");
-                }
-            } while (!((matches <= count) && (matches >= 1) && (matches <= 3)));
-            turn = !turn;
-            count -= matches;
+            System.out.println("Осталось спичек: " + count);
+            if (count >= 3) {
+                System.out.println(player + " введите число от 1 до 3:");
+            } else if (count == 2) {
+                System.out.println(player + " введите число от 1 до 2:");
+            } else {
+                System.out.println(player + ", введите 1, чтобы победить!");
+            }
+            matches = Integer.parseInt(input.nextLine());
+            if (!((matches <= count) && (matches >= 1) && (matches <= 3))) {
+                System.out.println("Неверный ход!");
+            } else {
+                turn = !turn;
+                count -= matches;
+            }
         }
         if (!turn) {
             System.out.println("Выиграл первый игрок");

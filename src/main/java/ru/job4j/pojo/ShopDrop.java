d61114c455571793fd89fd64e8e0f65beea8892c
diac <nikolai.gladkikh@gmail.com>
2022-08-13 08:34:07 +0300

    Исправить ошибки стиля: 3.2. Удаление моделей из массива. [#310016 #331402]

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -3,9 +3,8 @@ package ru.job4j.pojo;
 public class ShopDrop {
 
     public static Product[] delete(Product[] products, int index) {
-        while (index < products.length - 1) {
+        for (int i = index; i < products.length - 1; i++) {
             products[index] = products[index + 1];
-            index++;
         }
         products[products.length - 1] = null;
         return products;

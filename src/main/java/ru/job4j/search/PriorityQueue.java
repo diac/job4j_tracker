c7e3f977c7f26285b0dc11403667bd5da7f4c476
diac <nikolai.gladkikh@gmail.com>
2022-08-18 12:46:24 +0300

    Оптимизировать код: 2. Очередь с приоритетом на LinkedList [#41670 #333374]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 /**
  * Класс описывает работу простейшей очереди по приоритету, которая работает
  * по принципу FIFO (first in - first out)
+ *
  * @author STAS KOROBEYNIKOV
  * @version 1.0
  */
@@ -19,22 +20,23 @@ public class PriorityQueue {
      * Метод принимает на вход заявку и добавляет ее в очередь.
      * Если встречаются 2 задания с одинаковым приоритетом, то в очереди
      * они распределяются по принципу FIFO.
+     *
      * @param task задача которая добавляется в очередь
      */
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() <= task.getPriority()) {
-                index++;
-            } else {
+            if (element.getPriority() > task.getPriority()) {
                 break;
             }
+            index++;
         }
         this.tasks.add(index, task);
     }
 
     /**
      * Метод позволяет получить первую задачу в очереди
+     *
      * @return возвращает задачу из головы очереди или null если очередь пуста
      */
     public Task take() {

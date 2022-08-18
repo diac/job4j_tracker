920a07363e08c1e951ca3078e09688f322223e03
diac <nikolai.gladkikh@gmail.com>
2022-08-18 08:43:54 +0300

    Добавить дублирующие строки: 0. Set, HashSet, Iterator. [#10095 #333424]

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -10,6 +10,12 @@ public class UsageHashSet {
         autos.add("BMW");
         autos.add("Volvo");
         autos.add("Toyota");
+        autos.add("Lada");
+        autos.add("BMW");
+        autos.add("Volvo");
+        autos.add("Lada");
+        autos.add("BMW");
+        autos.add("Toyota");
         for (String auto : autos) {
             System.out.println(auto);
         }

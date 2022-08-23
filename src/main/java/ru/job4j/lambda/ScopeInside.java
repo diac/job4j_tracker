53dcd99c938b81f4a588a1de7a52acad5293a00c
diac <nikolai.gladkikh@gmail.com>
2022-08-23 10:30:21 +0300

    Заменить цикл на foreach: 4. Зона видимости в лямбда-выражениях и исключения [#70609 #335178]

--- src/main/java/ru/job4j/lambda/ScopeInside.java
+++ src/main/java/ru/job4j/lambda/ScopeInside.java
@@ -7,8 +7,7 @@ public class ScopeInside {
     public static void main(String[] args) {
         int[] number = {1, 2, 3};
         int total = 0;
-        for (int i = 0; i < number.length; i++) {
-            int num = number[i];
+        for (var num : number) {
             final int totalTemp = total;
             total = add(
                     () -> totalTemp + num

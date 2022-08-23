ef7d43e846d94465632e08016c1942df072c4dec
diac <nikolai.gladkikh@gmail.com>
2022-08-23 10:27:35 +0300

    Заменить анонимные классы лямбдами: 2.2. Функциональный интерфейс [#247497 #335138]

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -10,12 +10,7 @@ public class FunctionalInterfaces {
 
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
-        BiConsumer<Integer, String> biCon = new BiConsumer<>() {
-            @Override
-            public void accept(Integer index, String str) {
-                map.put(index, str);
-            }
-        };
+        BiConsumer<Integer, String> biCon = (index, str) -> map.put(index, str);
         biCon.accept(1, "one");
         biCon.accept(2, "two");
         biCon.accept(3, "three");
@@ -24,36 +19,17 @@ public class FunctionalInterfaces {
         biCon.accept(6, "six");
         biCon.accept(7, "seven");
 
-        BiPredicate<Integer, String> biPred = new BiPredicate<>() {
-            @Override
-            public boolean test(Integer index, String str) {
-                return index % 2 == 0 || str.length() == 4;
-            }
-        };
+        BiPredicate<Integer, String> biPred
+                = (index, str) -> index % 2 == 0 || str.length() == 4;
         for (Integer i : map.keySet()) {
             if (biPred.test(i, map.get(i))) {
                 System.out.println("key: " + i + " value: " + map.get(i));
             }
         }
-        Supplier<List<String>> sup = new Supplier<>() {
-            @Override
-            public List<String> get() {
-                return new ArrayList<>(map.values());
-            }
-        };
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
         List<String> strings = sup.get();
-        Consumer<String> con = new Consumer<>() {
-            @Override
-            public void accept(String s) {
-                System.out.println(s);
-            }
-        };
-        Function<String, String> func = new Function<>() {
-            @Override
-            public String apply(String s) {
-                return s.toUpperCase();
-            }
-        };
+        Consumer<String> con = s -> System.out.println(s);
+        Function<String, String> func = s -> s.toUpperCase();
         for (String s : strings) {
             con.accept(func.apply(s));
         }

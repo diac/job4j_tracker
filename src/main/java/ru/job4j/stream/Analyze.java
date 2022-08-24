1d4233564ea15bbe083c989425e45bd1b8c95eb7
diac <nikolai.gladkikh@gmail.com>
2022-08-24 09:42:03 +0300

    Убрать ссылку на LinkedHashMap::new из groupingBy: 2. Аттестация [#49777 #335577]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -61,7 +61,6 @@ public class Analyze {
         ).collect(
                 Collectors.groupingBy(
                         Subject::name,
-                        LinkedHashMap::new,
                         Collectors.summingDouble(Subject::score)
                 )
         ).entrySet().stream()
